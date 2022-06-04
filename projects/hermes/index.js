468545afc50903a5207b7dddb0162a5d0b1dcd61
a.maik <poseidonisanog@gmail.com>
2022-06-04 21:09:59 +0200

    Update index.js

--- projects/hermes/index.js
+++ projects/hermes/index.js
@@ -1,8 +1,10 @@
 const sdk = require("@defillama/sdk");
 const { getCompoundV2Tvl } = require("../helper/compound");
 const { staking } = require("../helper/staking");
-const hermesBar = "0x0000000000000000000000000000000000000000";
-const hermesToken = "0x0000000000000000000000000000000000000000";
+const hermesBar = "0x28A4E128f823b1b3168f82F64Ea768569a25a37F";
+const sHermes = "0x8812420fb6E5d971C969CcEF2275210AB8D014f0";
+const hermesToken = "0xba4476a302f5bc1dc4053cf79106dc43455904a3";
+//LENDING
 const comptroller = "0x0000000000000000000000000000000000000000";
 const { getChainTvl } = require("../helper/getUniSubgraphTvl");
 const graphUrls = {
@@ -18,6 +20,7 @@ module.exports = {
   harmony: {
     tvl: sdk.util.sumChainTvls([
       getChainTvl(graphUrls, "factories", "liquidityUSD")("harmony"),
+      //LENDING
       getCompoundV2Tvl(
         comptroller,
         "harmony",
@@ -26,6 +29,7 @@ module.exports = {
         "0x0000000000000000000000000000000000000000"
       ),
     ]),
+    //LENDING
     borrowed: getCompoundV2Tvl(
       comptroller,
       "harmony",
@@ -34,6 +38,8 @@ module.exports = {
       "0x0000000000000000000000000000000000000000",
       true
     ),
-    staking: staking(hermesBar, hermesToken, "harmony"),
+    staking:
+      staking(hermesBar, hermesToken, "harmony") +
+      staking(sHermes, hermesToken, "harmony"),
   },
 };
